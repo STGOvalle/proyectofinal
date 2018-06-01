b74ac63cd5f108ada38c885511772c0f2d437431
GothausseN <aliveordead@live.cl>
2018-06-01 13:59:41 -0400

    Agrega Login

---
+++ ProyectoFinal - Nasa/src/Interfaz/Login.java
@@ -0,0 +1,96 @@
+package Interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.event.InternalFrameAdapter;
+import javax.swing.event.InternalFrameEvent;
+
+public class Login extends JInternalFrame {
+	
+	JPanel jPrincipal, jPanelIzq, jPanelDer, jUser, jPass;
+	JLabel lUser, lPass;
+	JTextField tUser, tPass;
+	JButton btn_Login, btn_Invitado;
+	
+	public Login() {
+		super("Inicio de sesion");
+		
+		// Creacion de Paneles
+		//jPrincipal = new JPanel();
+		
+		jPanelIzq = new JPanel();
+		jPanelIzq.setLayout(null);
+		
+		/** Paneles Izquierdos **/
+		//jUser = new JPanel();
+		//jPass = new JPanel();
+		
+		jPanelDer = new JPanel();
+		
+		// Panel izquierdo
+		lUser = new JLabel("Usuario:");
+		lUser.setBounds(40, 20, 60, 20);
+		lPass = new JLabel("Contraseña:");
+		lPass.setBounds(40, 50, 80, 20);
+		tUser = new JTextField();
+		tUser.setBounds(150, 20, 100, 20);
+		tPass = new JTextField();
+		tPass.setBounds(150, 50, 100, 20);
+		
+		btn_Login = new JButton("Iniciar");
+		btn_Login.setBounds(40, 80, 100, 30);
+		
+		btn_Login.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Principal P = (Principal)getDesktopPane().getTopLevelAncestor();
+				P.mInicio.setVisible(true);
+				dispose(); /** Cerrar internal Frame **/
+			}
+		});
+		
+		btn_Invitado = new JButton("Invitado");
+		btn_Invitado.setBounds(150, 80, 100, 30);
+		
+
+		jPanelIzq.add(lUser);
+		jPanelIzq.add(tUser);
+		jPanelIzq.add(lPass);
+		jPanelIzq.add(tPass);
+		jPanelIzq.add(btn_Login);
+		jPanelIzq.add(btn_Invitado);
+		
+		
+		// Agregar a  Panel principal
+		
+		
+		add(jPanelIzq, BorderLayout.CENTER);
+		setVisible(true);
+		this.dibuja_frame();
+	}
+
+	public void dibuja_frame() {
+		// Asigna tamaño y posicionamiento
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension d = tk.getScreenSize();
+		
+		//asigna ancho y alto
+		int frame_anchura = d.width;
+		int frame_altura = d.height;
+		
+		setSize(300,170);
+		//pack();
+		setLocation(frame_anchura / 6, frame_altura / 6);
+	}
+}
---
+++ ProyectoFinal - Nasa/src/Interfaz/Principal.java
@@ -0,0 +1,113 @@
+package Interfaz;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.UIManager;
+
+public class Principal extends JFrame{
+	
+	private JDesktopPane DP;
+	private JMenuBar barra;
+	public JMenu mInicio;
+	public JMenuItem iCerrar;
+	
+	public Principal() {
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		/** Poner Icono **/
+		this.poner_icono();
+		
+		
+		/** Creación del Desktop **/
+		DP = new JDesktopPane() {
+			ImageIcon icon = new ImageIcon("imagenes/wallpaper-nasa.jpg");
+			Image image = icon.getImage();
+			
+			
+			@Override
+			protected void paintComponent(Graphics  g) {
+				super.paintComponent(g);
+				g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+			}
+		};
+		
+		this.setTitle("NASA CHILE"); /** Setea el titulo **/
+		this.setContentPane(DP); /** Setea el Desktop **/
+		
+		// Creacion de la barra del menu
+		barra = new JMenuBar();
+		this.setJMenuBar(barra);
+		
+		// Creacion de los Menu
+		mInicio = new JMenu("Inicio");
+		
+		mInicio.setVisible(false); /** Menu inicia invisible, se habilitará solo cuando se inicie sesion **/
+		
+		// Creacion de los Items
+		/** Pestaña Inicio **/
+		iCerrar = new JMenuItem("Cerrar Sesion");
+		
+		mInicio.add(iCerrar);
+		
+		//Agregar menu a la barra	
+		barra.add(mInicio);
+		
+		this.abrir_Login();
+		
+	}
+	
+	public void dibuja_frame() {
+		// Asigna tamaño y posicionamiento
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension d = tk.getScreenSize();
+		
+		//asigna ancho y alto
+		int frame_anchura = d.width;
+		int frame_altura = d.height;
+		
+		setSize(frame_anchura / 2, frame_altura / 2);
+		//pack();
+		setLocation(frame_anchura / 4, frame_altura / 4);
+	}
+	
+	public static void main(String [] args) {
+		
+		try {
+			JFrame.setDefaultLookAndFeelDecorated(true);
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		Principal P = new Principal();
+		P.dibuja_frame();
+		P.setVisible(true);
+	}
+	
+	public void poner_icono() {
+		ImageIcon icon = new ImageIcon("imagenes/nasa-ico.png");
+		Image icono = icon.getImage();
+		
+		this.setIconImage(icono);
+	}
+	
+	public void abrir_Login() {
+		Login L = new Login();
+		DP.add(L);
+	}
+}
